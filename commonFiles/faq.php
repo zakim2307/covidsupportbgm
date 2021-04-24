f81c7a5bd44970828bd9b221ea85ee5deea33b21
Zaki Mulla <zakim2307@gmail.com>
2021-04-24 11:22:47 +0530

    nudging faqs, final commit

--- commonFiles/faq.php
+++ commonFiles/faq.php
@@ -19,52 +19,72 @@
     </div><!-- /.accordion-item-header -->
     <div id="collapse3" class="collapse show" data-parent="#accordion">
         <div class="accordion__body">
-        <p>With any financial product that you buy, it is important that you know you are getting the best
-            advice from a reputable company as often</p>
+        <p>COVID-19 is a new disease, caused by a novel (or new) coronavirus that has not
+                                previously been seen in humans. Because it is a new virus, scientists are learning more
+                                each day. Although most people who have COVID-19 have mild symptoms, COVID-19 can also
+                                cause severe illness and even death. Some groups, including older adults and people who
+                                have certain underlying medical conditions, are at increased risk of severe illness.</p>
         </div><!-- /.accordion-item-body -->
     </div>
     </div><!-- /.accordion-item -->
     <div class="accordion-item">
     <div class="accordion__header" data-toggle="collapse" data-target="#collapse1">
-        <a class="accordion__title" href="#">Which Plan Is Right For Me?</a>
+        <a class="accordion__title" href="#">Why its called COVID-19?</a>
     </div><!-- /.accordion-item-header -->
     <div id="collapse1" class="collapse" data-parent="#accordion">
         <div class="accordion__body">
-        <p>With any financial product that you buy, it is important that you know you are getting the best
-            advice from a reputable company as often</p>
+        <p>On February 11, 2020 the World Health Organization announced an official name for the
+                                disease that is causing the 2019 novel coronavirus outbreak, first identified in Wuhan
+                                China. The new name of this disease is coronavirus disease 2019, abbreviated as
+                                COVID-19. In COVID-19, “CO” stands for corona, “VI” for virus, and ”D” for disease.
+                                Formerly, this disease was referred to as “2019 novel coronavirus” or “2019-nCoV.”</p>
         </div><!-- /.accordion-item-body -->
     </div>
     </div><!-- /.accordion-item -->
     <div class="accordion-item">
     <div class="accordion__header" data-toggle="collapse" data-target="#collapse2">
-        <a class="accordion__title" href="#">Do I have to commit to a contract?</a>
+        <a class="accordion__title" href="#">Does mask use help determine if someone is considered a close contact?</a>
     </div><!-- /.accordion-item-header -->
     <div id="collapse2" class="collapse" data-parent="#accordion">
         <div class="accordion__body">
-        <p>With any financial product that you buy, it is important that you know you are getting the best
-            advice from a reputable company as often</p>
+        <p>A person is still considered a close contact even if one or both people wore a mask when
+                                they were together.
+
+                                Masks work best when everyone wears one. However, masks are not 100% effective at
+                                preventing spread of COVID-19, and the type of masks used and whether or not they are
+                                used consistently and correctly varies throughout the general population. Therefore,
+                                mask use is not considered when determining whether someone is a close contact during
+                                case investigation and contact tracing.</p>
         </div><!-- /.accordion-item-body -->
     </div>
     </div><!-- /.accordion-item -->
     <div class="accordion-item">
     <div class="accordion__header" data-toggle="collapse" data-target="#collapse4">
-        <a class="accordion__title" href="#">What if I pick the wrong plan?</a>
+        <a class="accordion__title" href="#">what is the Difference between cleaning and disinfecting?</a>
     </div><!-- /.accordion-item-header -->
     <div id="collapse4" class="collapse" data-parent="#accordion">
         <div class="accordion__body">
-        <p>With any financial product that you buy, it is important that you know you are getting the best
-            advice from a reputable company as often</p>
+        <p>Cleaning with soap and water or a detergent removes germs, dirt, and impurities from
+                                surfaces. It lowers the risk of spreading infection. Disinfecting with a household
+                                disinfectant on List N: Disinfectants for Coronavirus (COVID-19)external icon kills
+                                germs on the surface. By disinfecting or killing germs on a surface after cleaning the
+                                surface, it can further lower the risk of spreading infection. For more information
+                                review cleaning and disinfection recommendations for facilities and homes.</p>
         </div><!-- /.accordion-item-body -->
     </div>
     </div><!-- /.accordion-item -->
     <div class="accordion-item">
     <div class="accordion__header" data-toggle="collapse" data-target="#collapse5">
-        <a class="accordion__title" href="#">Any contracts or commitments?</a>
+        <a class="accordion__title" href="#">who is COnsidered a close contact of someone with COVID-19?</a>
     </div><!-- /.accordion-item-header -->
     <div id="collapse5" class="collapse" data-parent="#accordion">
         <div class="accordion__body">
-        <p>With any financial product that you buy, it is important that you know you are getting the best
-            advice from a reputable company as often</p>
+        <p>For COVID-19, a close contact is anyone who was within 6 feet of an infected person for
+                                a total of 15 minutes or more over a 24-hour period (for example, three individual
+                                5-minute exposures for a total of 15 minutes). An infected person can spread COVID-19
+                                starting from 2 days before they have any symptoms (or, if they are asymptomatic, 2 days
+                                before their specimen that tested positive was collected), until they meet the criteria
+                                for discontinuing home isolation.</p>
         </div><!-- /.accordion-item-body -->
     </div>
     </div><!-- /.accordion-item -->
