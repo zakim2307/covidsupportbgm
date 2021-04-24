e12bd763d7dd10e592a6105ed042eb10ff79a868
Zaki Mulla <zakim2307@gmail.com>
2021-04-24 11:33:52 +0530

    nudging faqs, final commit

--- FAQ.php
+++ FAQ.php
@@ -49,8 +49,11 @@ Features Layout 1
     <div class="container">
         <div class="row mb-40">
             <div class="col-sm-12 col-md-12 col-lg-12">
-                <h4 class="">Frequently Asked Questions:
-                </h4>
+            <h5 class="text-block__title">FAQs, Helpfull COVID-19 Tips & Info</h5>
+              <p class="text-block__desc mb-20">We help create an awareness care plan that addresses your specific condition and we
+                are here to answer all of your questions & acknowledge your concerns. We try not only to provide outstanding care and treatment, but improving the
+                outcomes.
+              </p>
                 <!-- <div class="d-flex flex-wrap align-items-center mt-40 mb-30">
           <a href="appointment.html" class="btn btn__primary btn__rounded mr-30">
             <span>Make Appointment</span>
@@ -81,7 +84,7 @@ Features Layout 1
                     </div> -->
             <!-- </div>/.feature-item -->
             <!-- </div>/.col-lg-3 -->
-            <div class="col-sm-12 col-md-12 col-lg-12">
+            <!-- <div class="col-sm-12 col-md-12 col-lg-12">
                 <div class="accordion" id="accordionExample">
                     <div class="card">
                         <div class="card-header" id="headingOne">
@@ -190,7 +193,84 @@ Features Layout 1
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
+            <div id="accordion" class="mb-70">
+    <div class="accordion-item opened">
+    <div class="accordion__header" data-toggle="collapse" data-target="#collapse3">
+        <a class="accordion__title" href="#">What is COVID-19?</a>
+    </div><!-- /.accordion-item-header -->
+    <div id="collapse3" class="collapse show" data-parent="#accordion">
+        <div class="accordion__body">
+        <p>COVID-19 is a new disease, caused by a novel (or new) coronavirus that has not
+                                previously been seen in humans. Because it is a new virus, scientists are learning more
+                                each day. Although most people who have COVID-19 have mild symptoms, COVID-19 can also
+                                cause severe illness and even death. Some groups, including older adults and people who
+                                have certain underlying medical conditions, are at increased risk of severe illness.</p>
+        </div><!-- /.accordion-item-body -->
+    </div>
+    </div><!-- /.accordion-item -->
+    <div class="accordion-item">
+    <div class="accordion__header" data-toggle="collapse" data-target="#collapse1">
+        <a class="accordion__title" href="#">Why its called COVID-19?</a>
+    </div><!-- /.accordion-item-header -->
+    <div id="collapse1" class="collapse" data-parent="#accordion">
+        <div class="accordion__body">
+        <p>On February 11, 2020 the World Health Organization announced an official name for the
+                                disease that is causing the 2019 novel coronavirus outbreak, first identified in Wuhan
+                                China. The new name of this disease is coronavirus disease 2019, abbreviated as
+                                COVID-19. In COVID-19, “CO” stands for corona, “VI” for virus, and ”D” for disease.
+                                Formerly, this disease was referred to as “2019 novel coronavirus” or “2019-nCoV.”</p>
+        </div><!-- /.accordion-item-body -->
+    </div>
+    </div><!-- /.accordion-item -->
+    <div class="accordion-item">
+    <div class="accordion__header" data-toggle="collapse" data-target="#collapse2">
+        <a class="accordion__title" href="#">Does mask use help determine if someone is considered a close contact?</a>
+    </div><!-- /.accordion-item-header -->
+    <div id="collapse2" class="collapse" data-parent="#accordion">
+        <div class="accordion__body">
+        <p>A person is still considered a close contact even if one or both people wore a mask when
+                                they were together.
+
+                                Masks work best when everyone wears one. However, masks are not 100% effective at
+                                preventing spread of COVID-19, and the type of masks used and whether or not they are
+                                used consistently and correctly varies throughout the general population. Therefore,
+                                mask use is not considered when determining whether someone is a close contact during
+                                case investigation and contact tracing.</p>
+        </div><!-- /.accordion-item-body -->
+    </div>
+    </div><!-- /.accordion-item -->
+    <div class="accordion-item">
+    <div class="accordion__header" data-toggle="collapse" data-target="#collapse4">
+        <a class="accordion__title" href="#">what is the Difference between cleaning and disinfecting?</a>
+    </div><!-- /.accordion-item-header -->
+    <div id="collapse4" class="collapse" data-parent="#accordion">
+        <div class="accordion__body">
+        <p>Cleaning with soap and water or a detergent removes germs, dirt, and impurities from
+                                surfaces. It lowers the risk of spreading infection. Disinfecting with a household
+                                disinfectant on List N: Disinfectants for Coronavirus (COVID-19)external icon kills
+                                germs on the surface. By disinfecting or killing germs on a surface after cleaning the
+                                surface, it can further lower the risk of spreading infection. For more information
+                                review cleaning and disinfection recommendations for facilities and homes.</p>
+        </div><!-- /.accordion-item-body -->
+    </div>
+    </div><!-- /.accordion-item -->
+    <div class="accordion-item">
+    <div class="accordion__header" data-toggle="collapse" data-target="#collapse5">
+        <a class="accordion__title" href="#">who is COnsidered a close contact of someone with COVID-19?</a>
+    </div><!-- /.accordion-item-header -->
+    <div id="collapse5" class="collapse" data-parent="#accordion">
+        <div class="accordion__body">
+        <p>For COVID-19, a close contact is anyone who was within 6 feet of an infected person for
+                                a total of 15 minutes or more over a 24-hour period (for example, three individual
+                                5-minute exposures for a total of 15 minutes). An infected person can spread COVID-19
+                                starting from 2 days before they have any symptoms (or, if they are asymptomatic, 2 days
+                                before their specimen that tested positive was collected), until they meet the criteria
+                                for discontinuing home isolation.</p>
+        </div><!-- /.accordion-item-body -->
+    </div>
+    </div><!-- /.accordion-item -->
+</div><!-- /#accordion -->
 
 
         </div><!-- /.row -->
